52f4f751c77ac628ba82e1581cfd8a5141983d85
devikakarpe <devikankarpe@gmail.com>
2023-03-08 14:52:25 +0530

    added logic for playing options

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,5 +1,9 @@
 public class SnakeLadder {
 
+    private static final int NO_PLAY = 0;
+    private static final int LADDER = 1;
+    private static final int SNAKE = 2;
+
     public static int getRandomInteger(int maximum, int minimum) {
         return ((int) (Math.random() * (maximum - minimum))) + minimum;
 
@@ -9,5 +13,22 @@ public class SnakeLadder {
 
         int startPosition = 0;
         int dice = getRandomInteger(7,1);
+            System.out.println(dice);
+            int checkPlay = (int) Math.floor(Math.random()*10)%3;
+            System.out.println(checkPlay);
+
+            switch(checkPlay) {
+                case NO_PLAY:
+                    break;
+
+                case LADDER:
+                    startPosition += dice;
+                    break;
+
+                case SNAKE:
+                    startPosition -= dice;
+                    break;
+            }
+            System.out.println(startPosition);
     }
 }
