5e98020cd6929659cc00472ce47a2853ff1b3581
devikakarpe <devikankarpe@gmail.com>
2023-03-08 14:48:49 +0530

    added logic for rolling die to get number between 1 to 6

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,7 +1,13 @@
 public class SnakeLadder {
-    public static void main (String[] args) {
+
+    public static int getRandomInteger(int maximum, int minimum) {
+        return ((int) (Math.random() * (maximum - minimum))) + minimum;
+
+    }
+        public static void main (String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
 
-        int startPositionA = 0;
+        int startPosition = 0;
+        int dice = getRandomInteger(7,1);
     }
 }
