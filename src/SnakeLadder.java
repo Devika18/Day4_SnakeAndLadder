a94c21aa68c2fe27502432c4d105e00fecd2ca5d
devikakarpe <devikankarpe@gmail.com>
2023-03-08 14:44:54 +0530

    added logic for single player

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,5 +1,7 @@
 public class SnakeLadder {
     public static void main (String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
+
+        int startPositionA = 0;
     }
 }
